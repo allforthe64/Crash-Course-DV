9e4b42a452ae897931ba09824ee22faddc39413b
will <williamhrainey@gmail.com>
2022-01-17 13:41:20 -0800

    added axis labels, a plot label, and enlarged the size of tick lables

--- scatter_squares.py
+++ scatter_squares.py
@@ -4,5 +4,10 @@ plt.scatter(2, 4, s=200)
 
 #Set the chart title and label axes
 plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+#set the size of the tick lables
+plt.tick_params(axis="both", which="major", labelsize=14)
 
 plt.savefig("Output.png")
