b948e6fecd3d142fa840d78cc2ed7ff1275e5575
will <williamhrainey@gmail.com>
2022-01-18 11:53:10 -0800

    added functionality to generate multiple walks

--- rw_visualization.py
+++ rw_visualization.py
@@ -2,10 +2,15 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-#make a random walk and plot the points
-rw = RandomWalk()
-rw.fillWalk()
+#keep making random walks while the program is running
+while True:
+    rw = RandomWalk()
+    rw.fillWalk()
 
-plt.scatter(rw.xValues, rw.yValues, s=1)
-plt.savefig("RandWalk.png")
+    plt.scatter(rw.xValues, rw.yValues, s=1)
+    plt.savefig("RandWalk.png")
+
+    keepRunning = input("Make another walk? (y/n): ")
+    if keepRunning == "n":
+        break
 
