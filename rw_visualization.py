d1699daf151b5c294e7d33a0219bd4f40a6c54ed
will <williamhrainey@gmail.com>
2022-01-18 11:57:04 -0800

    added colormap to the scatter plot function

--- rw_visualization.py
+++ rw_visualization.py
@@ -7,7 +7,9 @@ while True:
     rw = RandomWalk()
     rw.fillWalk()
 
-    plt.scatter(rw.xValues, rw.yValues, s=1)
+    #add color map and plot points
+    pointNumbers = list(range(rw.numPoints))
+    plt.scatter(rw.xValues, rw.yValues, c=pointNumbers, cmap=plt.cm.viridis, s=1)
     plt.savefig("RandWalk.png")
 
     keepRunning = input("Make another walk? (y/n): ")
