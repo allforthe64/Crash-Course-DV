793f854363ef1b358f2db80a7aaa91ffaa55fd0c
will <williamhrainey@gmail.com>
2022-01-18 15:27:06 -0800

    added plotting function

--- high_lows.py
+++ high_lows.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 #get TMAX out of file
 filename = "sitka_weather_2018_full.csv"
@@ -17,5 +18,16 @@ with open(filename) as f:
             pass
         else:
             highsActual.append(int(i))
+
+    #plot data
+    fig = plt.figure(dpi=128, figsize=(10,6))
+    plt.plot(highsActual, c="red")
+
+    #format plot
+    plt.title("Daily high temperatures, July, 2018", fontsize=24)
+    plt.xlabel("", fontsize=16)
+    plt.ylabel("Temparature (F)", fontsize=16)
+    plt.tick_params(axis="both", which="major", labelsize=16)
     
-print(highsActual)
+    plt.savefig("high_temps.png")
+
