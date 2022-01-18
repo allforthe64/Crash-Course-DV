a2edaa41753684de1692094503d6a5264e16baed
will <williamhrainey@gmail.com>
2022-01-18 12:02:32 -0800

    added emphasis to the first and last points of every plot

--- rw_visualization.py
+++ rw_visualization.py
@@ -12,6 +12,10 @@ while True:
     plt.scatter(rw.xValues, rw.yValues, c=pointNumbers, cmap=plt.cm.viridis, s=1)
     plt.savefig("RandWalk.png")
 
+    #add emphasis to the first and last points
+    plt.scatter(0, 0, c="green", edgecolors="none", s=100)
+    plt.scatter(rw.xValues[-1], rw.yValues[-1], c="red", edgecolors="none", s=100)
+
     keepRunning = input("Make another walk? (y/n): ")
     if keepRunning == "n":
         break
