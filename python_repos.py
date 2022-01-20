c8d29d3ff4ab9fb3c524e6c95aa677e676754672
will <williamhrainey@gmail.com>
2022-01-20 10:20:20 -0800

    added code to examine the first repo

--- python_repos.py
+++ python_repos.py
@@ -7,6 +7,15 @@ print(f"Status code: {r.status_code}")
 
 #store api response in a variable
 responseDict = r.json()
+print(f"Total repositories: {responseDict['total_count']}")
 
-#process results
-print(responseDict.keys())
+#explore information about the repositories
+repoDicts = responseDict["items"]
+print(f"Repositories returned: {len(repoDicts)}")
+
+#examine the first repository
+repoDict1 = repoDicts[0]
+print("\nKeys: ", len(repoDict1))
+for key in repoDict1.keys():
+    print(key)
+
